726788bac54686beb55bec0dbfc27e2d12614010
Adexe Reyes <adexe.reyes.cb@gmail.com>
2023-10-20 19:03:03 +0100

    instalación básica

---
+++ Player.js
@@ -0,0 +1,11 @@
+class Player {
+    name;
+
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+// Esto son pruebas (ELIMINAR A PARTIR DE AQUÍ)
+let player1 = new Player("Juan");
+console.log(player1);
