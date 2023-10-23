de8eb652a2037bb0d6bc61c104778eabbd858815
Adexe Reyes <adexe.reyes.cb@gmail.com>
2023-10-23 17:17:32 +0100

    cambios a match done

--- Match.js
+++ Match.js
@@ -1,4 +1,12 @@
 class Match {
+    players = [];
+    winner = null;
 
+    addPlayer(player) {
+        if (this.players.length >= 2) return false
+        
+        this.players.push(player);
+        return true;
+    }
 }
 module.exports = Match;
--- Match.test.js
+++ Match.test.js
@@ -1,8 +1,45 @@
 const Match = require('./Match');
+const Player = require('./Player');
 
 test('Crear un partido', () => {
     const match1 = new Match();
-    expect(match1).toEqual({});
-    expect(match1 instanceof Match).toBe();
-    
+    expect(match1).toEqual({players: [], winner: null});
+    expect(match1 instanceof Match).toBeTruthy();
+});
+
+test('Insertar un jugador en un partido sin jugadores', () =>{
+    const match1 = new Match();
+    const player1 = new Player('Pepe');
+    expect(match1.addPlayer(player1)).toBeTruthy();
+    // expect(match1.players[0]).toBe(player1);
+    expect(match1.players.length).toBe(1);
+    expect(match1.players).toContain(player1);
+
+});
+test('Insertar un jugador en un partido con un jugador', () =>{
+    const match1 = new Match();
+    const player1 = new Player('Pepe');
+    expect(match1.addPlayer(player1)).toBeTruthy();
+    const player2 = new Player('Juan');
+    expect(match1.addPlayer(player2)).toBeTruthy();
+    // expect(match1.players[0]).toBe(player1);
+    // expect(match1.players[1]).toBe(player2);
+    expect(match1.players.length).toBe(2);
+    expect(match1.players).toContain(player1);
+    expect(match1.players).toContain(player2);
+});
+
+test('Insertar 3 jugadores, solo deben estar los 2 primeros, el último no se inserta', () => {
+    const match1 = new Match();
+    const player1 = new Player('Pepe');
+    expect(match1.addPlayer(player1)).toBeTruthy();
+    const player2 = new Player('Juan');
+    expect(match1.addPlayer(player2)).toBeTruthy();
+    const player3 = new Player('Ana');
+    expect(match1.addPlayer(player3)).toBeFalsy();
+    expect(match1.players.length).toBe(2);
+    expect(match1.players).toContain(player1);
+    expect(match1.players).toContain(player2);
+    expect(match1.players).not.toContain(player3);
+
 });
