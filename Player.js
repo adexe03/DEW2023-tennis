a85c688b8c0c95eb7ac2140beead201743177bd4
Adexe Reyes <adexe.reyes.cb@gmail.com>
2023-10-23 18:41:42 +0100

    cambios Player done

--- Player.js
+++ Player.js
@@ -1,11 +1,16 @@
 class Player {
     name;
     ranking;
+    #gender;
 
-    constructor(name, ranking = 0) {
+    constructor(name, ranking = 0, gender = 'female') {
         this.name = name;
         this.ranking = ranking;
+        if (gender == 'male') this.#gender = 'male';
+        else this.#gender = 'female';
     }
+
+    get gender() {return this.#gender;}
 }
 
 module.exports= Player;
--- Player.test.js
+++ Player.test.js
@@ -13,3 +13,27 @@ test('Crear jugador con raking', () => {
     expect(player1.ranking).toBe(9);
     expect(player1).toEqual({name: 'Pepe', ranking: 9});
 });
+
+test('Crear jugador male', () =>{
+    const player1 = new Player('Pepe', 5, 'male');
+    expect(player1.name).toBe('Pepe');
+    expect(player1.gender).toBe('male');
+});
+
+test('Crear jugador female', () =>{
+    const player1 = new Player('Ana', 3, 'female');
+    expect(player1.name).toBe('Ana');
+    expect(player1.gender).toBe('female');
+});
+
+test('Crear jugador female sin indicar el género', () =>{
+    const player1 = new Player('Ana');
+    expect(player1.name).toBe('Ana');
+    expect(player1.gender).toBe('female');
+});
+
+test('Crear jugador female por poner un género extraño', () =>{
+    const player1 = new Player('Ana', 2, 'pato');
+    expect(player1.name).toBe('Ana');
+    expect(player1.gender).toBe('female');
+});
